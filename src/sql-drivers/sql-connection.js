014ba8bd4764ca4028a49e852d5271cf0f059b94
Aaron Hoffman <ajh269@uakron.edu>
2022-10-25 22:11:04 -0400

    connections

--- src/sql-drivers/sql-connection.js
+++ src/sql-drivers/sql-connection.js
@@ -1,12 +1,33 @@
+// To solve issue with connection: https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+
+/*
+If issue arrises, run:
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your password';
+FLUSH PRIVILEGES;
+
+*/
+
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+var createConn = function()
+{
+    let conn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "RMMLSQL!Mani"
+    });
+    
+    conn.connect(function(err) {
+        if (err) {
+            throw err;
+        }
+        console.log(`Connected as ${conn.threadId}`);
+    });
+
+    return conn;
+}
+
+module.exports = {
+    createConn: createConn
+};
