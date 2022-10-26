f7e5127de0fdd85283a7a6db874891d1b3fa6f10
Aaron Hoffman <ajh269@uakron.edu>
2022-10-25 22:56:53 -0400

    basic alter setup

---
+++ src/sql-drivers/sql-alter.js
@@ -0,0 +1,6 @@
+// Drivers for ADD/REMOVE/UPDATE for a given table
+//! For midterm demo, focused on ADD a new issue
+
+//add is just a "new" button
+//remove is a right click issue & delete button
+//update is a right click issue & edit button
