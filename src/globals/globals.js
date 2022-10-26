35005349c101ef79c393155067189cc25a9ab94a
Aaron Hoffman <ajh269@uakron.edu>
2022-10-25 22:11:40 -0400

    added sensitive sql keywords

---
+++ src/globals/globals.js
@@ -0,0 +1,27 @@
+const sqlKeywords = [
+    'ADD',
+    'ALTER',
+    'COLUMN',
+    'TABLE',
+    'AS',
+    'CHECK',
+    'CREATE',
+    'DATABASE',
+    'DELETE',
+    'DISTINCT',
+    'DROP',
+    'EXISTS',
+    'JOIN',
+    'INSERT',
+    'IS NULL',
+    'IS NOT NULL',
+    'TABLE',
+    'UNION',
+    'UPDATE',
+    'WHERE'
+]
+
+
+module.exports = {
+    sqlKeywords
+};
