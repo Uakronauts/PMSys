f647ac6e26e5f8523af5e4e0d13933527c6b95b2
Aaron Hoffman <ajh269@uakron.edu>
2022-10-25 22:11:20 -0400

    basic query setup

---
+++ src/sql-drivers/sql-query.js
@@ -0,0 +1,24 @@
+// Query Syntax
+// -----
+/*
+*SYNTAX*:
+subsystem/system:
+priority:
+type:
+author: 
+assignee: 
+includes:
+is: {open/closed}
+
+*ORDER BY*
+datetime (asc/desc)
+author (alpha/rev alpha)
+priority (high to low/low to high)
+
+!SQL KEYWORDS ARE SENSITIVE!
+
+*/
+// 
+
+const { sqlKeywords } = require('../globals/globals');
+
