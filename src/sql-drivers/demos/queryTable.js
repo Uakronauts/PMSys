09f1e050b44e13f2acf33190d78543fde0a402e1
Aaron Hoffman <ajh269@uakron.edu>
2022-10-27 14:55:42 -0400

    couple more examples

--- src/sql-drivers/demos/queryTable.js
+++ src/sql-drivers/demos/queryTable.js
@@ -3,7 +3,21 @@ var { createConn } = require('../sql-connection');
 let conn = createConn('testDB');
 
 let sql = `
-INSERT INTO testTable VALUES ('Chris', 5)
+SELECT name
+FROM testtable
+WHERE id > 1
+`
+
+conn.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    console.log("Value Created", result);
+  });
+
+
+sql = `
+SELECT *
+FROM testtable
+ORDER BY name ASC
 `
 
 conn.query(sql, function (err, result, fields) {
